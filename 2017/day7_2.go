9926dd7902ca439d7ef1ae1adc1a04671c39d0ee
Küzdi Tamás <kuzditomi@gmail.com>
2017-12-09 14:31:01 +0100

    day 8

--- 2017/day7_2.go
+++ 2017/day7_2.go
@@ -1423,20 +1423,6 @@ tnuvco (67) -> jkqctlo, jborbs
 kyshwy (60)
 baomcxj (54)`
 
-var puzzle2 = `pbga (66)
-xhth (57)
-ebii (61)
-havc (66)
-ktlj (57)
-fwft (72) -> ktlj, cntj, xhth
-qoyq (66)
-padx (45) -> pbga, havc, qoyq
-tknk (41) -> ugml, padx, fwft
-jptl (61)
-ugml (68) -> gyxo, ebii, jptl
-gyxo (61)
-cntj (57)`
-
 type program struct {
 	name        string
 	parent      *program
